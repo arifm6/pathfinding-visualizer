6bc976563351ce377e8010aaeb553843aca39434
Ahmad Arif <arifmassih6@gmail.com>
2023-03-08 01:31:38 -0500

    finished tutorial

--- src/components/Tutorial.jsx
+++ src/components/Tutorial.jsx
@@ -14,10 +14,44 @@ export default function Tutorial() {
       />
     );
   });
+  const endOfTutorial = (
+    <div className="flex flex-col space-y-4 text-center  grow  ">
+      <h1 className="text-md md:text-xl lg:text-3xl font-bold text-[#7A003C]">
+        End of Tutorial{" "}
+      </h1>
+      <h2 className="text-sm md:text-base lg:text-lg font-medium  text-[#5E6A71]">
+        Feel free to use the application and play with it as you wish
+      </h2>
+      <p className="text-xs md:text-sm lg:text-base">
+        The source code can be found at my{" "}
+        <a
+          href="https://github.com/arifm6"
+          className="text-[#FDBF57]"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          github
+        </a>{" "}
+        or on my{" "}
+        <a
+          href="https://ahmadarif.com"
+          className="text-[#FDBF57]"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          website
+        </a>
+      </p>
+    </div>
+  );
+  tutorialItems.push(endOfTutorial);
 
   return (
     tutorialItemsPointer < tutorialItems.length && (
-      <div className="fixed w-[70vw] md:w-[50vw] h-[70vh]   mx-auto top-[7.5rem] left-[15%] md:left-[25%] border-2 border-[#7A003C] bg-white z-[999] rounded-md p-4 flex flex-col justify-between space-y-1 ">
+      <div className="fixed w-[70vw] md:w-[50vw] h-[70vh]   mx-auto top-[5rem] lg:top-[7.5rem] left-[15%] md:left-[25%] border-2 border-[#7A003C] bg-white z-[999] rounded-md p-4 flex flex-col justify-between space-y-1 ">
+        <div className="absolute right-[2%] top-[2%] text-xs">{`${
+          tutorialItemsPointer + 1
+        }/${tutorialItems.length}`}</div>
         {tutorialItems[tutorialItemsPointer]}
         <div className="flex justify-between ">
           <button
@@ -37,50 +71,18 @@ export default function Tutorial() {
                 Previous
               </button>
             )}
-            {tutorialItemsPointer < tutorialItems.length - 1 && (
+            {
               <button
                 className="tutorial-button"
                 onClick={() => setTutorialItemsPointer((prev) => prev + 1)}
               >
-                Next
+                {tutorialItemsPointer < tutorialItems.length - 1
+                  ? "Next"
+                  : "Finish"}
               </button>
-            )}
+            }
           </div>
         </div>
-        {/**
-        <button
-          className="absolute bottom-4 left-4 py-1 sm:py-2 px-2 sm:px-4 rounded-md bg-[#FDBF57] opacity-90 hover:opacity-80 active:opacity-100 text-[2vw] sm:text-lg"
-          onClick={() => setTutorialItemsPointer(tutorialItems.length)}
-        >
-          Skip Tutorial
-        </button>
-        <div className="flex space-x-1 lg:space-x-6 absolute bottom-4 right-4 ">
-          {tutorialItemsPointer > 0 && (
-            <button
-              className="bg-[#FDBF57] py-1 sm:py-2 px-2 sm:px-4 rounded-md opacity-90 hover:opacity-80 active:opacity-100 text-[2vw] sm:text-lg"
-              onClick={() =>
-                setTutorialItemsPointer((previousValue) => previousValue - 1)
-              }
-            >
-              Previous
-            </button>
-          )}
-          {tutorialItemsPointer < tutorialItems.length && (
-            <button
-              className="bg-[#FDBF57] py-1 sm:py-2 px-2 sm:px-4 rounded-md opacity-90 hover:opacity-80 active:opacity-100 text-[2vw] sm:text-lg"
-              onClick={() =>
-                setTutorialItemsPointer((previousValue) => {
-                  return previousValue + 1;
-                })
-              }
-            >
-              {tutorialItemsPointer < tutorialItems.length - 1
-                ? "Next"
-                : "Finish"}
-            </button>
-          )}
-          
-        </div> */}
       </div>
     )
   );
--- src/tutorialInfo.js
+++ src/tutorialInfo.js
@@ -26,4 +26,55 @@ export default [
     image: "./algorithms-dropdown.png",
     imageAlt: "algorithms-dropdown",
   },
+  {
+    header: "Bidirectional Algorithms",
+    subheader:
+      "You can select whether you would like to search bidirectionally or not.",
+    description:
+      "Bidirectional search uses both the start and end point to try and find a path that meet in the middle. Although they can be just as effective as their unidirectional counterpart, they may have trouble finding the shortest path when weights are involved.",
+    image: "./bidirectional.png",
+    imageAlt: "bidirectional",
+  },
+  {
+    header: "Mazes",
+    subheader:
+      "You can choose from a maze generation algorithm to quickly develop a prebuilt board",
+    description:
+      "There are a variety of maze generation algorithms that you can use to build a board and test our algorithms.",
+    image: "./maze.png",
+    imageAlt: "maze",
+  },
+  {
+    header: "Adding a carrot",
+    subheader: 'Click "Add Carrot" ',
+    description:
+      "You can add a carrot to the board to have an additional end destination. The algorithm will first try to find the closest path to the carrot node (from the start), and will then look for the closest path to the finish node (from the carrot)",
+    image: "./carrot.png",
+    imageAlt: "carrot",
+  },
+  {
+    header: "Obstacles",
+    subheader:
+      "Click (and drag) on the grid to add an obstacle. You can change the current obstacle in the navigation bar ",
+    description:
+      "Walls block paths and weights have an added cost to traverse through that node. It costs 16 units to pass through a weight and 1 unit to pass through an empty node",
+    image: "./obstacles.gif",
+    imageAlt: "obstacles animation",
+  },
+  {
+    header: "Dragging Nodes",
+    subheader: "Click (and drag) permanent nodes to move them ",
+    description:
+      "You can drag, add, or remove nodes after an algorithm has finished running to instantly see the new results",
+    image: "./dragging-nodes.gif",
+    imageAlt: "dragging nodes",
+  },
+  {
+    header: "Other features",
+    subheader: "Use the navigation bar to explore all the possible features  ",
+    description:
+      "You can also reset the board, change the animation speed, and visualize the pathfinding algorithms using the navigation bar. Some of these interact with each other so feel free to explore and try different combinations!",
+    image: "./other-features.png",
+    imageAlt: "other features navigation bar",
+  },
 ];
